42599a64540950030d1a0dbd014622fef82492fd
hiba77moh <hiba77moh@gmail.com>
2025-02-13 01:31:16 -0800

    creating items

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -3,11 +3,29 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use App\Models\todo ;
+
 
 class TodoList extends Component
 {
+    #[Rule(['required', 'min:2'])]
+
+    public $name;
+
+
+    public function create(){
+        $validate=$this->validateOnly('name');
+        todo::create($validate);
+        $this->reset('name');
+        request()->session()->flash('success' ,'created successfully');
+    }
+
+
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list',[
+            'todo' => todo::latest()->get()
+        ]);
     }
 }
